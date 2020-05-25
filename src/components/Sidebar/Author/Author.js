f55009acd0ea3d36f770fe0cb17b16099dcc9616
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-25 17:08:34 +0100

    added native lazy loading to author image

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -7,30 +7,35 @@ type Props = {
   author: {
     name: string,
     bio: string,
-    photo: string
+    photo: string,
   },
-  isIndex: ?boolean
+  isIndex: ?boolean,
 };
 
 const Author = ({ author, isIndex }: Props) => (
   <div className={styles['author']}>
-    <Link to="/">
+    <Link to='/'>
       <img
         src={author.photo}
+        loading='lazy'
         className={styles['author__photo']}
-        width="75"
-        height="75"
+        width='75'
+        height='75'
         alt={author.name}
       />
     </Link>
 
-    { isIndex === true ? (
+    {isIndex === true ? (
       <h1 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+        <Link className={styles['author__title-link']} to='/'>
+          {author.name}
+        </Link>
       </h1>
     ) : (
       <h2 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+        <Link className={styles['author__title-link']} to='/'>
+          {author.name}
+        </Link>
       </h2>
     )}
     <p className={styles['author__subtitle']}>{author.bio}</p>
