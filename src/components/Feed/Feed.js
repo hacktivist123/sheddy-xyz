2adacde329a85c99a2afc82464ecd97eb93ffda0
hacktivist123 <akintayoshedrack@gmail.com>
2020-10-09 17:49:49 +0100

    create new react hooks for handling external scripts

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -4,6 +4,7 @@ import moment from 'moment'
 import { Link } from 'gatsby'
 import type { Edges } from '../../types'
 import styles from './Feed.module.scss'
+import { useExternalLink } from '../../hooks'
 
 type Props = {
   edges: Edges,
@@ -12,44 +13,49 @@ type Props = {
 const Feed = ({ edges }: Props) => (
   <div className={styles['feed']}>
     <black-lives></black-lives>
-    {edges.map((edge) => (
-      <div className={styles['feed__item']} key={edge.node.fields.slug}>
-        <div className={styles['feed__item-meta']}>
-          <time
-            className={styles['feed__item-meta-time']}
-            dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}
-          >
-            {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
-          </time>
-          <span className={styles['feed__item-meta-divider']} />
-          <span className={styles['feed__item-meta-category']}>
-            <Link
-              to={edge.node.fields.categorySlug}
-              className={styles['feed__item-meta-category-link']}
+    {
+      (useExternalLink('https://unpkg.com/sars-stand/black-lives.js', 'module'),
+      edges.map((edge) => (
+        <div className={styles['feed__item']} key={edge.node.fields.slug}>
+          <div className={styles['feed__item-meta']}>
+            <time
+              className={styles['feed__item-meta-time']}
+              dateTime={moment(edge.node.frontmatter.date).format(
+                'MMMM D, YYYY'
+              )}
             >
-              {edge.node.frontmatter.category}
+              {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
+            </time>
+            <span className={styles['feed__item-meta-divider']} />
+            <span className={styles['feed__item-meta-category']}>
+              <Link
+                to={edge.node.fields.categorySlug}
+                className={styles['feed__item-meta-category-link']}
+              >
+                {edge.node.frontmatter.category}
+              </Link>
+            </span>
+          </div>
+          <h2 className={styles['feed__item-title']}>
+            <Link
+              className={styles['feed__item-title-link']}
+              to={edge.node.fields.slug}
+            >
+              {edge.node.frontmatter.title}
             </Link>
-          </span>
-        </div>
-        <h2 className={styles['feed__item-title']}>
+          </h2>
+          <p className={styles['feed__item-description']}>
+            {edge.node.frontmatter.description}
+          </p>
           <Link
-            className={styles['feed__item-title-link']}
+            className={styles['feed__item-readmore']}
             to={edge.node.fields.slug}
           >
-            {edge.node.frontmatter.title}
+            Read
           </Link>
-        </h2>
-        <p className={styles['feed__item-description']}>
-          {edge.node.frontmatter.description}
-        </p>
-        <Link
-          className={styles['feed__item-readmore']}
-          to={edge.node.fields.slug}
-        >
-          Read
-        </Link>
-      </div>
-    ))}
+        </div>
+      )))
+    }
   </div>
 )
 
